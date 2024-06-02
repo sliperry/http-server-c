4c0f20f6b117a00bfcedaeaa507d8eb6c0aaff1c
sliperry <116978110+sliperry@users.noreply.github.com>
2024-06-02 07:43:01 +0300

    Respond with 200

--- app/server.c
+++ app/server.c
@@ -1,11 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <string.h>
-#include <errno.h>
-#include <unistd.h>
+#include "server.h"
 
 int main() {
 	// Disable output buffering
@@ -17,12 +10,12 @@ int main() {
 
 	// Uncomment this block to pass the first stage
 	int server_fd, client_addr_len;
-	struct sockaddr_in client_addr;
+	struct sockaddr_in client_addr, serv_addr;
 	
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd == -1) {
 		printf("Socket creation failed: %s...\n", strerror(errno));
-		return 1;
+		exit(EXIT_FAILURE);
 	}
 	
 	// Since the tester restarts your program quite often, setting SO_REUSEADDR
@@ -30,32 +23,42 @@ int main() {
 	int reuse = 1;
 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
 		printf("SO_REUSEADDR failed: %s \n", strerror(errno));
-		return 1;
+		exit(EXIT_FAILURE);
 	}
 	
-	struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
-									 .sin_port = htons(4221),
-									 .sin_addr = { htonl(INADDR_ANY) },
-									};
+	serv_addr = { 	.sin_family = AF_INET ,
+					.sin_port = htons(PORT),
+					.sin_addr = { htonl(INADDR_ANY) },
+				};
 	
 	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
 		printf("Bind failed: %s \n", strerror(errno));
-		return 1;
+		exit(EXIT_FAILURE);
 	}
 	
-	int connection_backlog = 5;
+	int connectio n_backlog = 5;
 	if (listen(server_fd, connection_backlog) != 0) {
 		printf("Listen failed: %s \n", strerror(errno));
-		return 1;
+		exit(EXIT_FAILURE);
 	}
 	
 	printf("Waiting for a client to connect...\n");
 	client_addr_len = sizeof(client_addr);
 	
-	accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+	int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);	
+	if (client_fd == -1) {
+		printf("Accept failed: %s \n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
 	printf("Client connected\n");
+
+	char buffer[BUFFERSIZE];
+	char *message = "HTTP/1.1 200 OK\r\n\r\n";
+	while( read(client_fd, buffer, BUFFERSIZE) != 0) {
+		send(client_fd, message, strlen(message));
+	}
 	
 	close(server_fd);
 
-	return 0;
+	exit(EXIT_SUCCESS);
 }
---
+++ app/server.h
@@ -0,0 +1,16 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define BUFFERSIZE 1024
+#define PORT 4221 
+
+#endif // SERVER_Hx
